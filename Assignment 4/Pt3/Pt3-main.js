c62cfe06b0bd08644606b49f83e1607d4d5fa731
saadat176 <68792187+saadat176@users.noreply.github.com>
2025-08-01 23:28:13 -0400

    canvas added to HTML, Ball class and context constants initialized in JS

---
+++ Assignment 4/Pt3/Pt3-main.js
@@ -0,0 +1,13 @@
+/*
+Name: Saadat Baig
+File: Pt3-main.js
+Date: 01 August 2025
+Starter JavaScript for object building practice (Part 3)
+*/
+
+// Canvas setup
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
