54aa3885bcea4d44e7c27e038aecf72d357d5897
saadat176 <68792187+saadat176@users.noreply.github.com>
2025-08-01 23:31:35 -0400

    Added draw, update, and collisionDetect methods to Ball prototype

--- Assignment 4/Pt3/Pt3-main.js
+++ Assignment 4/Pt3/Pt3-main.js
@@ -11,3 +11,59 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+// Utility functions
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// Ball constructor
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+// Draw method
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+// Update method
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// Collision detection method
+Ball.prototype.collisionDetect = function () {
+  for (let i = 0; i < balls.length; i++) {
+    if (!(this === balls[i])) {
+      const dx = this.x - balls[i].x;
+      const dy = this.y - balls[i].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[i].size) {
+        balls[i].color = this.color = randomRGB();
+      }
+    }
+  }
+};
