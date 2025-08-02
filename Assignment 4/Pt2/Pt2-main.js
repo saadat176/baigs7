4dfbd0563c2c914a6f6e43c43f5525d6a92b9ff8
saadat176 <68792187+saadat176@users.noreply.github.com>
2025-08-01 21:29:34 -0400

    added images

--- Assignment 4/Pt2/Pt2-main.js
+++ Assignment 4/Pt2/Pt2-main.js
@@ -13,11 +13,11 @@ const overlay = document.querySelector('.overlay');
 
 // Image file names and alt text
 const imageList = [
-  { src: 'images/pic1.jpg', alt: 'Closeup of a human eye' },
-  { src: 'images/pic2.jpg', alt: 'Rock that looks like a wave' },
-  { src: 'images/pic3.jpg', alt: 'Purple and white pansies' },
-  { src: 'images/pic4.jpg', alt: 'Section of wall from a pharaoh’s tomb' },
-  { src: 'images/pic5.jpg', alt: 'Large moth on a leaf' }
+  { src: 'Pt2/images/pic1.jpg', alt: 'Closeup of a human eye' },
+  { src: 'Pt2/images/pic2.jpg', alt: 'Rock that looks like a wave' },
+  { src: 'Pt2/images/pic3.jpg', alt: 'Purple and white pansies' },
+  { src: 'Pt2/images/pic4.jpg', alt: 'Section of wall from a pharaoh’s tomb' },
+  { src: 'Pt2/images/pic5.jpg', alt: 'Large moth on a leaf' }
 ];
 
 // Loop through imageList and create thumbnails
