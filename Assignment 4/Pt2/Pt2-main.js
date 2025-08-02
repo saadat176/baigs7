0f1c6d09dd2f8a553be80905f56c656da4cb7826
saadat176 <68792187+saadat176@users.noreply.github.com>
2025-08-01 20:39:04 -0400

    Added base HTML, CSS, and JavaScript files for image gallery project. Included MDN layout and structure. Placeholder JS file added.

---
+++ Assignment 4/Pt2/Pt2-main.js
@@ -0,0 +1,8 @@
+/*
+Name: Saadat Baig
+File: Pt2-main.js
+Date: 01 August 2025
+JavaScript logic for building a dynamic image gallery (base structure).
+*/
+
+// Variables will be added in later commits
